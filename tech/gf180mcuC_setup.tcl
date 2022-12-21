59de805922d21a3b34079023d8c6a1659e40f347
D. Mitch Bailey <d.mitch.bailey@gmail.com>
2022-12-20 17:00:35 -0800

    Aligned with most recent gf180mcu pdk

--- tech/gf180mcuC_setup.tcl
+++ tech/gf180mcuC_setup.tcl
@@ -6,6 +6,9 @@ permute default
 property default
 property parallel none
 
+# Allow override of default #columns in the output format.
+catch {format $env(NETGEN_COLUMNS)}
+
 #---------------------------------------------------------------
 # For the following, get the cell lists from
 # circuit1 and circuit2.
@@ -22,28 +25,45 @@ set cells2 [cells list -all -circuit2]
 # Resistors (except metal)
 #-------------------------------------------
 
-set devices {nwell_1p5 ppolyf_u npolyf_u ppolyf_s nplus_u pplus_u nw1a_6p0}
-lappend devices npolyf_s pfield_1p5 pf1va_6p0
-lappend devices ppolyf_u_1k ppolyf_u_1k_6p0
+set devices {}
+lappend devices nwell_1p5
+lappend devices ppolyf_u
+lappend devices npolyf_u
+lappend devices ppolyf_s
+lappend devices nplus_u
+lappend devices pplus_u
+lappend devices nw1a_6p0
+
+lappend devices npolyf_s
+lappend devices pfield_1p5
+lappend devices pf1va_6p0
+lappend devices ppolyf_u_1k
+lappend devices ppolyf_u_1k_6p0
 
 foreach dev $devices {
     if {[lsearch $cells1 $dev] >= 0} {
-	property "-circuit1 $dev" parallel enable
-	property "-circuit1 $dev" serial enable
 	permute "-circuit1 $dev" 1 2
-	property "-circuit1 $dev" merge {r_length ser_critical} {r_width add_critical}
+	property "-circuit1 $dev" series enable
+	property "-circuit1 $dev" series {r_width critical}
+	property "-circuit1 $dev" series {r_length add}
+	property "-circuit1 $dev" parallel enable
+	property "-circuit1 $dev" parallel {r_length critical}
+	property "-circuit1 $dev" parallel {r_width add}
 	property "-circuit1 $dev" tolerance {r_length 0.01} {r_width 0.01}
 	# Ignore these properties
-	property "-circuit2 $dev" delete par1 pm
+	property "-circuit1 $dev" delete par1 pm par r
     }
     if {[lsearch $cells2 $dev] >= 0} {
-	property "-circuit2 $dev" parallel enable
-	property "-circuit2 $dev" serial enable
 	permute "-circuit2 $dev" 1 2
-	property "-circuit2 $dev" merge {r_length ser_critical} {r_width add_critical}
+	property "-circuit2 $dev" series enable
+	property "-circuit2 $dev" series {r_width critical}
+	property "-circuit2 $dev" series {r_length add}
+	property "-circuit2 $dev" parallel enable
+	property "-circuit2 $dev" parallel {r_length critical}
+	property "-circuit2 $dev" parallel {r_width add}
 	property "-circuit2 $dev" tolerance {r_length 0.01} {r_width 0.01}
 	# Ignore these properties
-	property "-circuit2 $dev" delete par1 pm
+	property "-circuit2 $dev" delete par1 pm par r
     }
 }
 
@@ -51,50 +71,178 @@ foreach dev $devices {
 # RM (metal) resistors
 #-------------------------------------------
 
-set devices {rm1 rm2}
+set devices {}
+lappend devices rm1
+lappend devices rm2
 lappend devices rm3
 lappend devices rm4
 lappend devices rmtp
 
 foreach dev $devices {
     if {[lsearch $cells1 $dev] >= 0} {
-	property "-circuit1 $dev" parallel enable
 	permute "-circuit1 $dev" 1 2
-	property "-circuit1 $dev" tolerance {l 0.01} {w 0.01}
+	property "-circuit1 $dev" series enable
+	property "-circuit1 $dev" series {r_width critical}
+	property "-circuit1 $dev" series {r_length add}
+	property "-circuit1 $dev" parallel enable
+	property "-circuit1 $dev" parallel {r_length critical}
+	property "-circuit1 $dev" parallel {r_width add}
+	property "-circuit1 $dev" tolerance {r_length 0.01} {r_width 0.01}
 	# Ignore these properties
-	property "-circuit2 $dev" delete par1 pm
+	property "-circuit1 $dev" delete par1 pm par r
     }
     if {[lsearch $cells2 $dev] >= 0} {
-	property "-circuit2 $dev" parallel enable
 	permute "-circuit2 $dev" 1 2
-	property "-circuit2 $dev" tolerance {l 0.01} {w 0.01}
+	property "-circuit2 $dev" series enable
+	property "-circuit2 $dev" series {r_width critical}
+	property "-circuit2 $dev" series {r_length add}
+	property "-circuit2 $dev" parallel enable
+	property "-circuit2 $dev" parallel {r_length critical}
+	property "-circuit2 $dev" parallel {r_width add}
+	property "-circuit2 $dev" tolerance {r_length 0.01} {r_width 0.01}
 	# Ignore these properties
-	property "-circuit2 $dev" delete par1 pm
+	property "-circuit2 $dev" delete par1 pm par r
+    }
+}
+
+#------------------------------------------------------------------
+# (MOS) transistors
+#------------------------------------------------------------------
+
+set devices {}
+lappend devices nfet_03v3
+lappend devices pfet_03v3
+lappend devices nfet_06v0
+lappend devices pfet_06v0
+lappend devices nfet_06v0_nvt
+
+foreach dev $devices {
+    if {[lsearch $cells1 $dev] >= 0} {
+	permute "-circuit1 $dev" 1 3
+	property "-circuit1 $dev" parallel enable
+	property "-circuit1 $dev" parallel {l critical}
+	property "-circuit1 $dev" parallel {w add}
+	property "-circuit1 $dev" tolerance {w 0.01} {l 0.01}
+	# Ignore these properties
+	property "-circuit1 $dev" delete par1 NRD NRS par
+	property "-circuit1 $dev" delete sa sb sd par dtemp nf
+	property "-circuit1 $dev" delete as ad ps pd
+    }
+    if {[lsearch $cells2 $dev] >= 0} {
+	permute "-circuit2 $dev" 1 3
+	property "-circuit2 $dev" parallel enable
+	property "-circuit2 $dev" parallel {l critical}
+	property "-circuit2 $dev" parallel {w add}
+	property "-circuit2 $dev" tolerance {w 0.01} {l 0.01}
+	# Ignore these properties
+	property "-circuit2 $dev" delete par1 NRD NRS par
+	property "-circuit2 $dev" delete sa sb sd par dtemp nf
+	property "-circuit2 $dev" delete as ad ps pd
+    }
+}
+
+#------------------------------------------------------------------
+# (MOS) salicide block extended source/drain transistors
+# (NOTE:  _dss devices are treated as symmetric because the
+#  salicide block is present on both sides of the gate.)
+#------------------------------------------------------------------
+
+set devices {}
+lappend devices nfet_03v3_dss
+lappend devices pfet_03v3_dss
+lappend devices nfet_06v0_dss
+lappend devices pfet_06v0_dss
+
+foreach dev $devices {
+    if {[lsearch $cells1 $dev] >= 0} {
+	permute "-circuit1 $dev" 1 3
+	property "-circuit1 $dev" parallel enable
+	property "-circuit1 $dev" parallel {l critical}
+	property "-circuit1 $dev" parallel {w add}
+	property "-circuit1 $dev" tolerance {w 0.01} {l 0.01}
+	# Ignore these properties
+	property "-circuit1 $dev" delete par1 NRD NRS par
+	property "-circuit1 $dev" delete sa sb sd par dtemp nf
+	property "-circuit1 $dev" delete as ad ps pd
+	property "-circuit1 $dev" associate {d_sab 1} {s_sab 3}
+    }
+    if {[lsearch $cells2 $dev] >= 0} {
+	permute "-circuit2 $dev" 1 3
+	property "-circuit2 $dev" parallel enable
+	property "-circuit2 $dev" parallel {l critical}
+	property "-circuit2 $dev" parallel {w add}
+	property "-circuit2 $dev" tolerance {w 0.01} {l 0.01}
+	# Ignore these properties
+	property "-circuit2 $dev" delete par1 NRD NRS par
+	property "-circuit2 $dev" delete sa sb sd par dtemp nf
+	property "-circuit2 $dev" delete as ad ps pd
+	property "-circuit2 $dev" associate {d_sab 1} {s_sab 3}
     }
 }
 
 #-------------------------------------------
-# (MOS) transistors
+# (MOS) transistors asymmetric source/drain
 #-------------------------------------------
 
-set devices {nmos_1p5 pmos_1p5 nmos_6p0 pmos_6p0 nmoscap_1p5 nmoscap_6p0}
+set devices {}
+lappend devices nfet_10v0_asym
+lappend devices pfet_10v0_asym
 
 foreach dev $devices {
     if {[lsearch $cells1 $dev] >= 0} {
 	property "-circuit1 $dev" parallel enable
-	permute "-circuit1 $dev" 1 3
-	property "-circuit1 $dev" merge {w add_critical}
+	property "-circuit1 $dev" parallel {l critical}
+	property "-circuit1 $dev" parallel {w add}
 	property "-circuit1 $dev" tolerance {w 0.01} {l 0.01}
 	# Ignore these properties
-	property "-circuit2 $dev" delete par1 NRD NRS
+	property "-circuit1 $dev" delete par1 NRD NRS
+	property "-circuit1 $dev" delete sa sb sd par dtemp nf
+	property "-circuit1 $dev" delete as ad ps pd
     }
     if {[lsearch $cells2 $dev] >= 0} {
 	property "-circuit2 $dev" parallel enable
-	permute "-circuit2 $dev" 1 3
-	property "-circuit2 $dev" merge {w add_critical}
+	property "-circuit2 $dev" parallel {l critical}
+	property "-circuit2 $dev" parallel {w add}
 	property "-circuit2 $dev" tolerance {w 0.01} {l 0.01}
 	# Ignore these properties
 	property "-circuit2 $dev" delete par1 NRD NRS
+	property "-circuit2 $dev" delete sa sb sd par dtemp nf
+	property "-circuit2 $dev" delete as ad ps pd
+    }
+}
+
+#-------------------------------------------
+# MOSCAP capacitors and varactors
+#-------------------------------------------
+
+set devices {}
+lappend devices cap_nmos_03v3
+lappend devices cap_nmos_06v0
+lappend devices cap_pmos_03v3
+lappend devices cap_pmos_06v0
+lappend devices cap_nmos_03v3_b
+lappend devices cap_nmos_06v0_b
+lappend devices cap_pmos_03v3_b
+lappend devices cap_pmos_06v0_b
+
+foreach dev $devices {
+    if {[lsearch $cells1 $dev] >= 0} {
+	property "-circuit1 $dev" parallel enable
+	property "-circuit1 $dev" parallel {c_length critical}
+	property "-circuit1 $dev" parallel {c_width add}
+	property "-circuit1 $dev" tolerance {c_width 0.01} {c_length 0.01}
+	# Ignore these properties
+	property "-circuit1 $dev" delete par1 NRD NRS
+	property "-circuit1 $dev" delete as ad ps pd
+    }
+    if {[lsearch $cells2 $dev] >= 0} {
+	property "-circuit2 $dev" parallel enable
+	property "-circuit2 $dev" parallel {c_length critical}
+	property "-circuit2 $dev" parallel {c_width add}
+	property "-circuit2 $dev" tolerance {c_width 0.01} {c_length 0.01}
+	# Ignore these properties
+	property "-circuit2 $dev" delete par1 NRD NRS
+	property "-circuit2 $dev" delete as ad ps pd
     }
 }
 
@@ -102,7 +250,16 @@ foreach dev $devices {
 # diodes
 #-------------------------------------------
 
-set devices {np_1p5 pn_1p5 np_6p0 pn_6p0 nwp_1p5 nwp_6p0}
+set devices {}
+lappend devices diode_nd2ps_03v3
+lappend devices diode_pd2nw_03v3
+lappend devices diode_nd2ps_06v0
+lappend devices diode_pd2nw_06v0
+lappend devices diode_nw2pw_03v3
+lappend devices diode_nw2pw_06v0
+lappend devices diode_dnw2pw
+lappend devices diode_dnw2ps
+lappend devices sc_diode
 
 foreach dev $devices {
     if {[lsearch $cells1 $dev] >= 0} {
@@ -110,14 +267,14 @@ foreach dev $devices {
 	property "-circuit1 $dev" merge {area add_critical}
 	property "-circuit1 $dev" tolerance {area 0.02}
 	# Ignore these properties
-	property "-circuit2 $dev" delete par1 peri
+	property "-circuit1 $dev" delete par1 pj
     }
     if {[lsearch $cells2 $dev] >= 0} {
 	property "-circuit2 $dev" parallel enable
 	property "-circuit2 $dev" merge {area add_critical}
 	property "-circuit2 $dev" tolerance {area 0.02}
 	# Ignore these properties
-	property "-circuit2 $dev" delete par1 peri
+	property "-circuit2 $dev" delete par1 pj
     }
 }
 
@@ -127,16 +284,25 @@ foreach dev $devices {
 # sandwich (MoM) capacitors, and MiM capacitors
 #-----------------------------------------------
 
-set devices {vnpn_lv_2p5x2p5 vnpn_lv_5x5 vnpn_lv_10x10}
-lappend devices vnpn_mv_2p5x2p5 vnpn_mv_5x5 vnpn_mv_10x10
-lappend devices apmom_bb
-lappend devices mim_sm_bb
+set devices {}
+lappend devices npn_10p00x10p00
+lappend devices npn_05p00x05p00
+lappend devices npn_00p54x16p00
+lappend devices npn_00p54x08p00
+lappend devices npn_00p54x04p00
+lappend devices npn_00p54x02p00
+lappend devices pnp_10p00x00p42
+lappend devices pnp_05p00x00p42
+lappend devices pnp_10p00x10p00
+lappend devices pnp_05p00x05p00
+
+lappend devices cap_mim_2f0_m4m5_noshield
 
 foreach dev $devices {
     if {[lsearch $cells1 $dev] >= 0} {
 	property "-circuit1 $dev" parallel enable
 	# Ignore these properties
-	property "-circuit2 $dev" delete par1
+	property "-circuit1 $dev" delete par1
     }
     if {[lsearch $cells2 $dev] >= 0} {
 	property "-circuit2 $dev" parallel enable
@@ -146,70 +312,107 @@ foreach dev $devices {
 }
 
 #---------------------------------------------------------------
-# D_CELLS_SC9 (ignore FILL cells)
+# Digital cells (ignore decap, fill, and tap cells)
+# Make a separate list for each supported library
 #---------------------------------------------------------------
-# e.g., ignore class "-circuit2 FILL5"
+# e.g., ignore class "-circuit2 gf180mcu_fd_sc_7t5v0__endcap"
 #---------------------------------------------------------------
 
 foreach cell $cells1 {
-    if {[regexp "FILL\[0-9\]+" $cell match]} {
+#   if {[regexp {gf180mcu_fd_sc_[^_]+__fillcap_[[:digit:]]+} $cell match]} {
+#       ignore class "-circuit1 $cell"
+#   }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__endcap} $cell match]} {
+        ignore class "-circuit1 $cell"
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__fill_[[:digit:]]+} $cell match]} {
+        ignore class "-circuit1 $cell"
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__filltie} $cell match]} {
         ignore class "-circuit1 $cell"
     }
 }
+
 foreach cell $cells2 {
-    if {[regexp "FILL\[0-9\]+" $cell match]} {
+#   if {[regexp {gf180mcu_fd_sc_[^_]+__fillcap_[[:digit:]]+} $cell match]} {
+#       ignore class "-circuit2 $cell"
+#   }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__endcap} $cell match]} {
+        ignore class "-circuit2 $cell"
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__fill_[[:digit:]]+} $cell match]} {
+        ignore class "-circuit2 $cell"
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__filltie} $cell match]} {
+        ignore class "-circuit2 $cell"
+    }
+}
+
+# Do the same for the OSU standard cell libraries
+
+foreach cell $cells1 {
+    if {[regexp {gf180mcu_osu_sc_[^_]+__fill_[[:digit:]]+} $cell match]} {
+        ignore class "-circuit1 $cell"
+    }
+}
+
+foreach cell $cells2 {
+    if {[regexp {gf180mcu_osu_sc_[^_]+__fill_[[:digit:]]+} $cell match]} {
         ignore class "-circuit2 $cell"
     }
 }
 
 #---------------------------------------------------------------
-# ICPIO_5P0 (ignore FILL cells)
+# Allow the fill, decap, etc., cells to be parallelized
 #---------------------------------------------------------------
-# e.g., ignore class "-circuit1 GF_CI_FILL5"
-#---------------------------------------------------------------
-foreach cell $cells1 {
-    if {[regexp "GF_CI_FILL.*" $cell match]} {
-        ignore class "-circuit1 $cell"
-    }
-}
-foreach cell $cells2 {
-    if {[regexp "GF_CI_FILL.*" $cell match]} {
-        ignore class "-circuit2 $cell"
-    }
-}
 
-#---------------------------------------------------------------
-# Handle cells captured from Electric
-#
-# Find cells of the form "<library>__<cellname>" in the netlist
-# from Electric where the extracted layout netlist has only
-# "<cellname>".  Cross-check by ensuring that the full name
-# "<library>__<cellname>" does not exist in both cells, and that
-# the truncated name "<cellname>" does not exist in both cells.
-#---------------------------------------------------------------
-# e.g., hydra_spi_controller__hydra_spi_controller
-#---------------------------------------------------------------
 foreach cell $cells1 {
-    if {[regexp "(.+)__(.+)" $cell match library cellname]} {
-        if {([lsearch $cells2 $cell] < 0) && \
-                ([lsearch $cells2 $cellname] >= 0) && \
-                ([lsearch $cells1 $cellname] < 0)} {
-            equate classes "-circuit1 $cell" "-circuit2 $cellname"
-	    puts stdout "Matching pins of $cell in circuit 1 and $cellname in circuit 2"
-	    equate pins "-circuit1 $cell" "-circuit2 $cellname"
-        }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__fillcap_[[:digit:]]+} $cell match]} {
+        property "-circuit1 $cell" parallel enable
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__endcap} $cell match]} {
+        property "-circuit1 $cell" parallel enable
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__fill_[[:digit:]]+} $cell match]} {
+        property "-circuit1 $cell" parallel enable
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__filltie} $cell match]} {
+        property "-circuit1 $cell" parallel enable
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__antenna} $cell match]} {
+        property "-circuit1 $cell" parallel enable
     }
 }
 
 foreach cell $cells2 {
-    if {[regexp "(.+)__(.+)" $cell match library cellname]} {
-        if {([lsearch $cells1 $cell] < 0) && \
-                ([lsearch $cells1 $cellname] >= 0) && \
-                ([lsearch $cells2 $cellname] < 0)} {
-            equate classes "-circuit1 $cellname" "-circuit2 $cell"
-	    puts stdout "Matching pins of $cellname in circuit 1 and $cell in circuit 2"
-	    equate pins "-circuit1 $cellname" "-circuit2 $cell"
-        }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__fillcap_[[:digit:]]+} $cell match]} {
+        property "-circuit2 $cell" parallel enable
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__endcap} $cell match]} {
+        property "-circuit2 $cell" parallel enable
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__fill_[[:digit:]]+} $cell match]} {
+        property "-circuit2 $cell" parallel enable
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__filltie} $cell match]} {
+        property "-circuit2 $cell" parallel enable
+    }
+    if {[regexp {gf180mcu_fd_sc_[^_]+__antenna} $cell match]} {
+        property "-circuit2 $cell" parallel enable
+    }
+}
+
+# Do the same for the OSU 3.3V standard cell library
+
+foreach cell $cells1 {
+    if {[regexp {gf180mcu_osu_sc_[^_]+__fill_[[:digit:]]+} $cell match]} {
+        property "-circuit1 $cell" parallel enable
+    }
+}
+
+foreach cell $cells2 {
+    if {[regexp {gf180mcu_osu_sc_[^_]+__fill_[[:digit:]]+} $cell match]} {
+        property "-circuit2 $cell" parallel enable
     }
 }
 
